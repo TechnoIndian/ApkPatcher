b68c0e526125b16deb710155232ab45c2e293298
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2024-12-11 19:23:15 +0530

    Add files via upload

--- ApkPatcher/Decompile_Compile.py
+++ ApkPatcher/Decompile_Compile.py
@@ -6,22 +6,21 @@ C=CM()
 from.Files_Check import FileCheck
 F=FileCheck()
 F.set_paths()
-apktool_path,key_path,cert_path=F.apktool_path,F.key_path,F.cert_path
 class De_Compiler:
 	def decompile_apk(E,apk_path,decompile_dir):
-		B=decompile_dir;A=apk_path;print(f"\n{C.r}_____________________________________________________________\n");D=[_B,_C,apktool_path,'d','-f',A,'-o',B];print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Decompiling APK...\n{C.g}  |\n  └──── {C.r}Decompiling ~{C.g}$ java -jar {C.os.path.basename(apktool_path)} d -f {apk_path} -o {C.os.path.basename(decompile_dir)}\n\n{C.r}_____________________________________________________________{C.g}\n")
-		try:C.subprocess.run(D,check=_A);print(f"\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Decompile Successful  {C.g}✔{C.r}\n\n{C.r}_____________________________________________________________\n\n")
+		B=apk_path;A=decompile_dir;print(f"\n{C.r}_____________________________________________________________\n");D=[_B,_C,F.apktool_path,'d','-f',B,'-o',A,'-p',A];print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Decompiling APK...");print(f"{C.g}  |\n  └──── {C.r}Decompiling ~{C.g}$ java -jar {C.os.path.basename(F.apktool_path)} d -f {B} -o {C.os.path.basename(A)}\n");print(f"{C.r}_____________________________________________________________{C.g}\n")
+		try:C.subprocess.run(D,check=_A);print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Decompile Successful  {C.g}✔{C.r}\n");print(f"{C.r}_____________________________________________________________\n\n")
 		except C.subprocess.CalledProcessError:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}] {C.rd} Decompile Failed ! ✘{C.r}\n")
 	def recompile_apk(E,decompile_dir,build_dir):
 		B=decompile_dir;A=build_dir
-		if C.os.path.isfile(A):print(f"{C.r}_____________________________________________________________\n\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} APK Already Exists.\n{C.g}  |\n  └──── {C.g}Removed Old APK... {C.y}{build_dir} {C.g}✔{C.r}\n");C.os.remove(A)
-		D=[_B,_C,apktool_path,'b',B,'-o',A];print(f"{C.r}_____________________________________________________________\n\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile APK...\n\n{C.g}  |\n  └──── {C.r}Recompiling with aapt ~{C.g}$ java -jar {C.os.path.basename(apktool_path)} b {C.os.path.basename(decompile_dir)} -o {C.os.path.basename(build_dir)}\n\n{C.r}_____________________________________________________________{C.g}\n\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} ApkTool Default...{C.g}\n\n")
-		try:C.subprocess.run(D,check=_A);print(f"\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile Successful  {C.g}✔{C.r}\n\n{C.r}_____________________________________________________________\n")
+		if C.os.path.isfile(A):print(f"{C.r}_____________________________________________________________\n");print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} APK Already Exists.\n{C.g}  |\n  └──── {C.g}Removed Old APK... {C.y}{A} {C.g}✔{C.r}\n");C.os.remove(A)
+		D=[_B,_C,F.apktool_path,'b',B,'-o',A,'-p',F.A_F_P];print(f"{C.r}_____________________________________________________________\n");print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile APK...");print(f"{C.g}  |\n  └──── {C.r}Recompiling ~{C.g}$ java -jar {C.os.path.basename(F.apktool_path)} b {C.os.path.basename(B)} -o {C.os.path.basename(A)}\n");print(f"{C.r}_____________________________________________________________{C.g}\n");print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} ApkTool Default...{C.g}\n")
+		try:C.subprocess.run(D,check=_A);print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile Successful  {C.g}✔{C.r}\n");print(f"{C.r}_____________________________________________________________\n")
 		except C.subprocess.CalledProcessError:
-			print(f"\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Default Recompile Failed! ✘{C.r}\n");D=[_B,_C,apktool_path,'b','-use-aapt2',B,'-o',A];print(f"{C.r}_____________________________________________________________\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile APK...\n\n{C.g}  |\n  └──── {C.r}Recompiling with aapt2 ~{C.g}$ java -jar {C.os.path.basename(apktool_path)} b -c -use-aapt2 {C.os.path.basename(decompile_dir)} -o {C.os.path.basename(build_dir)}\n\n{C.r}_____________________________________________________________{C.g}\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} ApkTool AAPT2...{C.g}\n")
-			try:C.subprocess.run(D,check=_A);print(f"\n\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile Successful with aapt2 {C.g} ✔{C.r}\n\n{C.r}_____________________________________________________________\n")
+			print(f"\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Default Recompile Failed! ✘{C.r}\n");D=[_B,_C,F.apktool_path,'b','-use-aapt2',B,'-o',A,'-p',F.A_F_P];print(f"{C.r}_____________________________________________________________\n");print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile APK...");print(f"{C.g}  |\n  └──── {C.r}Recompiling with aapt2 ~{C.g}$ java -jar {C.os.path.basename(F.apktool_path)} b -use-aapt2 {C.os.path.basename(B)} -o {C.os.path.basename(A)}\n");print(f"{C.r}_____________________________________________________________{C.g}\n");print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} ApkTool AAPT2...{C.g}\n")
+			try:C.subprocess.run(D,check=_A);print(f"\n{C.lb}[ {C.pr}* {C.lb}] {C.c} Recompile Successful with aapt2 {C.g} ✔{C.r}\n");print(f"{C.r}_____________________________________________________________\n")
 			except C.subprocess.CalledProcessError:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} AAPT2 Recompile Failed! ✘{C.r}\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Recompile Failed with both Default & aapt2 ! ✘{C.r}\n")
 	def align_apk(D,build_dir,out_dir):A=out_dir+'_aligned.apk';B=['zipalign','-v','4',build_dir,A];C.subprocess.run(B,stdout=C.subprocess.DEVNULL,stderr=C.subprocess.DEVNULL,check=_A);return A
-	def sign_apk(F,build_dir,out_dir):
-		B='true';A=out_dir;D=F.align_apk(build_dir,A);G=['apksigner','sign','--key',key_path,'--cert',cert_path,'--out',A,'--v1-signing-enabled',B,'--v2-signing-enabled',B,'--v3-signing-enabled',B,D];C.subprocess.run(G,check=_A);C.os.remove(D);E=A+'.idsig'
+	def sign_apk(G,build_dir,out_dir):
+		B='true';A=out_dir;D=G.align_apk(build_dir,A);H=['apksigner','sign','--key',F.key_path,'--cert',F.cert_path,'--out',A,'--v1-signing-enabled',B,'--v2-signing-enabled',B,'--v3-signing-enabled',B,D];C.subprocess.run(H,check=_A);C.os.remove(D);E=A+'.idsig'
 		if C.os.path.exists(E):C.os.remove(E)
--- ApkPatcher/Files_Check.py
+++ ApkPatcher/Files_Check.py
@@ -1,7 +1,7 @@
 from.C_M import CM
 C=CM()
 class FileCheck:
-	def set_paths(A):B=C.os.path.dirname(C.os.path.abspath(C.sys.argv[0]));A.apkeditor_path=C.os.path.join(B,'APKEditor.jar');A.apktool_path=C.os.path.join(B,'APKTool_AP.jar');A.key_path=C.os.path.join(B,'testkey.pk8');A.cert_path=C.os.path.join(B,'testkey.x509.pem');A.apktool_framework_zip=C.os.path.join(C.os.path.expanduser('~'),'ApkTool_Framwork.zip');A.local_folder_path=C.os.path.join(C.os.path.expanduser('~'),'.local')
+	def set_paths(A):D='.local/AP_Framework';B=C.os.path.dirname(C.os.path.abspath(C.sys.argv[0]));A.apkeditor_path=C.os.path.join(B,'APKEditor.jar');A.apktool_path=C.os.path.join(B,'APKTool_AP.jar');A.A_F_P=C.os.path.join(C.os.path.expanduser('~'),D);A.A_F=C.os.path.join(C.os.path.expanduser('~'),D,'1.apk');A.key_path=C.os.path.join(B,'testkey.pk8');A.cert_path=C.os.path.join(B,'testkey.x509.pem');A.AES_P=C.os.path.join(C.os.path.dirname(C.os.path.abspath(__file__)),'AES.smali')
 	def download_file(Q,jar_urls_and_paths):
 		import requests as G;J=set()
 		for(H,B)in jar_urls_and_paths:
@@ -16,15 +16,10 @@ class FileCheck:
 					print(f"\n{C.g}  |\n  └──── {C.r}~{C.g}$ Downloaded {A} Successfully. ✔\n")
 				else:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Failed to download {C.y}{A} {C.rd}Status Code: {D.status_code}\n\n{C.lb}[ {C.rd}INFO {C.lb}]{C.rd} Restart Script...{C.r}\n")
 			except G.exceptions.RequestException:exit(f'\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Got an error while Fetching {C.y}{local_path}\n\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} No internet Connection\n\n{C.lb}[ {C.rd}INFO {C.lb}]{C.rd} Internet connection is required to download {C.y}{lo_path}\n')
-	def unzip_apktool_framework(A):
-		if C.os.path.exists(A.apktool_framework_zip):
-			try:
-				B=C.os.path.expanduser('~');C.os.makedirs(B,exist_ok=True)
-				with C.zipfile.ZipFile(A.apktool_framework_zip,'r')as E:E.extractall(B);C.os.remove(A.apktool_framework_zip)
-				D=C.os.path.join(B,'ApkTool_Framwork-main')
-				if C.os.path.exists(D):C.os.rename(D,A.local_folder_path)
-			except(C.zipfile.BadZipFile,Exception)as F:exit(f"\n{C.lb}[ {C.rd}Error ! {C.lb}]{C.rd} Failed to unzip {C.y}{A.apktool_framework_zip}{C.r}\n")
 	def F_D(A):
-		D=[('https://github.com/TechnoIndian/RKPairip/releases/download/Editor/APKEditor.jar',A.apkeditor_path),('https://github.com/TechnoIndian/RKPairip/releases/download/Editor/apktool.jar'if C.os.name=='nt'else'https://github.com/TechnoIndian/RKPairip/releases/download/Editor/apktool_modified.jar',A.apktool_path),('https://github.com/TechnoIndian/ApkTool_Framwork/releases/download/ApkTool_2.10.0/testkey.pk8',A.key_path),('https://github.com/TechnoIndian/ApkTool_Framwork/releases/download/ApkTool_2.10.0/testkey.x509.pem',A.cert_path)];A.download_file(D);B=[]
-		if not C.os.path.exists(A.local_folder_path):B.append(('https://github.com/TechnoIndian/ApkTool_Framwork/archive/refs/heads/main.zip',A.apktool_framework_zip))
-		A.download_file(B);A.unzip_apktool_framework();C.os.system('cls'if C.os.name=='nt'else'clear')
+		B='nt';D=[('https://github.com/TechnoIndian/RKPairip/releases/download/Editor/APKEditor.jar',A.apkeditor_path),('https://github.com/TechnoIndian/RKPairip/releases/download/Editor/apktool.jar'if C.os.name==B else'https://github.com/TechnoIndian/RKPairip/releases/download/Editor/apktool_modified.jar',A.apktool_path),('https://github.com/TechnoIndian/ApkTool_Framwork/releases/download/APKTool_2.10.0/testkey.pk8',A.key_path),('https://github.com/TechnoIndian/ApkTool_Framwork/releases/download/APKTool_2.10.0/testkey.x509.pem',A.cert_path),('https://raw.githubusercontent.com/TechnoIndian/Objectlogger/refs/heads/main/AES.smali',A.AES_P)]
+		if C.os.name!=B:
+			E=C.os.path.dirname(A.A_F)
+			if not C.os.path.exists(E):C.os.makedirs(E)
+			D.append(('https://github.com/TechnoIndian/ApkTool_Framwork/releases/download/APKTool_2.10.0/1.apk',A.A_F))
+		A.download_file(D);C.os.system('cls'if C.os.name==B else'clear')
