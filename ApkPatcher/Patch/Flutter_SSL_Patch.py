ef7dd276e86b41042e5a70e0a7293e71cc1e14c1
TechnoIndian <techno india@users.noreply.github.com>
2026-05-10 06:34:35 +0530

    Improved flutter patch

--- ApkPatcher/Patch/Flutter_SSL_Patch.py
+++ ApkPatcher/Patch/Flutter_SSL_Patch.py
@@ -12,6 +12,7 @@ patterns = {
         "F. 43 01 D1 FE 67 01 A9 F8 5F 02 A9 F6 57 03 A9 F4 4F 04 A9 13 00 40 F9 F4 03 00 AA 68 1A 40 F9",
         "FF 43 01 D1 FE 67 01 A9 .. .. 06 94 .. 7. 06 94 68 1A 40 F9 15 15 41 F9 B5 00 00 B4 B6 4A 40 F9",
         "F. 0F 1C F8 F. .. 0. .. .. .. .. .9 .. .. 0. .. 68 1A 40 F9 15 .. 4. F9 B5 00 00 B4 B6 46 40 F9",
+        "FF .3 01 D1 F. .. 01 A9 .. .. .. 94 .. .. .. 52 48 00 00 39 1A 50 40 F9 DA 02 00 B4 48 03 40 F9", # ssl_crypto_x509_session_verify_cert_chain
     ],
     "arm": [
         "2D E9 F. 4. D0 F8 00 80 81 46 D8 F8 18 00 D0 F8",
@@ -21,18 +22,7 @@ patterns = {
         "55 41 57 41 56 41 55 41 54 53 50 49 89 F. 4. 8B .. 4. 8B 4. 30 4C 8B .. .. 0. 00 00 4D 85 .. 74 1. 4D 8B",
         "55 41 57 41 56 41 55 41 54 53 48 83 EC 18 49 89 FF 48 8B 1F 48 8B 43 30 4C 8B A0 28 02 00 00 4D 85 E4 74",
         "55 41 57 41 56 41 55 41 54 53 48 83 EC 18 49 89 FE 4C 8B 27 49 8B 44 24 30 48 8B 98 D0 01 00 00 48 85 DB",
-    ],
-}
-
-# ---------------- ssl_crypto_x509_session_verify_cert_chain ----------------
-patterns2 = {
-    "arm64": [
-        "FF .3 01 D1 F. .. 01 A9 .. .. .. 94 .. .. .. 52 48 00 00 39 1A 50 40 F9 DA 02 00 B4 48 03 40 F9",
-    ],
-    "arm": [
-    ],
-    "x86": [
-        "55 41 57 41 56 41 55 41 54 53 48 83 EC 38 C6 02 50 48 8B AF .. 00 00 00 48 85 ED 74 .. 48 83 7D 00 00 74 ..",
+        "55 41 57 41 56 41 55 41 54 53 48 83 EC 38 C6 02 50 48 8B AF .. 00 00 00 48 85 ED 74 .. 48 83 7D 00 00 74 ..", # ssl_crypto_x509_session_verify_cert_chain
     ],
 }
 
@@ -77,38 +67,27 @@ def find_offset(r2, patterns, is_iA=False):
         return
 
     if arch in patterns:
-        for arch in patterns:
-            for pattern in patterns[arch] + patterns2[arch]:
+        for idx, pattern in enumerate(patterns[arch]):
+            search_result = r2.cmd(f"/x {pattern}")
+            search_result = search_result.strip().split(" ")[0]
 
-                isX509_verify_cert = pattern in patterns2[arch]
+            if search_result:
+                print(f"\n{C.X}{C.C} Matched Pattern\n  |\n  ╰┈{idx}➢ {C.PN} {pattern}\n")
 
-                search_result = r2.cmd(f"/x {pattern}")
-                search_result = search_result.strip().split(" ")[0]
+                search_fcn = r2.cmd(f"{search_result};afl.").strip().split(" ")[0]
 
-                if search_result:
-                    print(f"\n{C.X}{C.C} Matched Pattern\n  |\n  ╰┈➢ {C.PN} {pattern}\n")
+                print(f"\n{C.X}{C.C} ssl_verify_peer_cert found at: {C.PN}{search_result}\n")
 
-                    search_fcn = r2.cmd(f"{search_result};afl.").strip().split(" ")[0]
+                if not search_fcn:
+                    search_fcn = search_result
+                    r2.cmd(f"af @{search_fcn}")
 
-                    if isX509_verify_cert:
-                        print(f"\n{C.X}{C.C} session_verify_cert_chain found at: {C.PN}{search_result}\n")
-                    else:
-                        print(f"\n{C.X}{C.C} ssl_verify_peer_cert found at: {C.PN}{search_result}\n")
+                print(f"\n{C.X}{C.C} function at: {C.PN}{search_fcn}\n")
 
-                    # session_verify_cert_chain fallback
-                    if not search_fcn and isX509_verify_cert:
-                        r2.cmd(f"af @{search_result}")
-                        search_fcn = search_result
+                patch_cmd = "wao ret1" if idx == 4 and arch in ("arm64", "x86") else "wao ret0"
 
-                    if not search_fcn and arch == "x86":
-                        search_fcn = search_result
-                        r2.cmd(f"af @{search_fcn}")
+                return search_fcn, patch_cmd
 
-                    print(f"\n{C.X}{C.C} function at: {C.PN}{search_fcn}\n")
-
-                    return search_fcn, isX509_verify_cert
-
-    return None, False
 
 # ---------------- Patch Flutter SSL ----------------
 def Patch_Flutter_SSL(decompile_dir, isAPKEditor):
@@ -160,16 +139,11 @@ def Patch_Flutter_SSL(decompile_dir, isAPKEditor):
 
     print(f"\n{C.X}{C.G} Searching for offset...\n")
 
-    offset, isX509_verify_cert = find_offset(r2, patterns, is_iA)
+    offset, patch_cmd = find_offset(r2, patterns, is_iA)
 
-    if offset and isX509_verify_cert:
+    if offset:
         r2.cmd(f"{offset}")
-        r2.cmd("wao ret1")
-        print(f"\n{C.X}{C.C} session_verify_cert_chain: {C.G}Patched Successfully  ✔\n")
-    
-    elif offset:
-        r2.cmd(f"{offset}")
-        r2.cmd("wao ret0")
+        r2.cmd(patch_cmd)
         print(f"\n{C.X}{C.C} ssl_verify_peer_cert: {C.G}Patched Successfully  ✔\n")
 
     else:
